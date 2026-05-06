55c0d1c9f671d5247f91322a9b0088563bf828bd
faketreez <215413555+faketreez@users.noreply.github.com>
2026-05-06 22:32:59 +1000

    add local emacs customizations

---
+++ lisp/init-local.el
@@ -0,0 +1,27 @@
+;;; init-local.el --- Local customizations -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;; Mac key modifiers
+(setq mac-command-modifier 'super)
+(setq mac-option-modifier 'meta)
+
+;; Font
+(set-face-attribute 'default nil :font "Iosevka" :height 160)
+
+;; Gruber Darker theme
+(require-package 'gruber-darker-theme)
+(setq-default custom-enabled-themes '(gruber-darker))
+
+;; Override the dark command to use gruber-darker
+(defun dark ()
+  "Activate a dark color theme."
+  (interactive)
+  (setq custom-enabled-themes '(gruber-darker))
+  (reapply-themes))
+
+;; Show line numbers in all file-editing buffers
+(global-display-line-numbers-mode 1)
+
+(provide 'init-local)
+;;; init-local.el ends here
